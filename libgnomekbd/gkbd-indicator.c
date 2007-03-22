92f9fa39766ab76642fcc7210bb52adaf39463b0
Sergey V. Udaltsov <svu@src.gnome.org>
2007-03-22 22:56:46 +0000

    fixing invalid free() issue, b.g.o#421252
    svn path=/trunk/; revision=67

--- libgnomekbd/gkbd-indicator.c
+++ libgnomekbd/gkbd-indicator.c
@@ -269,13 +269,18 @@ gkbd_indicator_prepare_drawing (GkbdIndicator * gki, int group)
 			    (full_layout_name, &layout_name,
 			     &variant_name))
 				/* just in case */
-				layout_name = g_strdup (full_layout_name);
+				layout_name = full_layout_name;
+
+			/* make it freeable */
+			layout_name = g_strdup (layout_name);
 
 			if (globals.short_group_names != NULL) {
 				char *short_group_name =
 				    globals.short_group_names[group];
 				if (short_group_name != NULL
 				    && *short_group_name != '\0') {
+					/* drop the long name */
+					g_free (layout_name);
 					layout_name =
 					    g_strdup (short_group_name);
 				}
