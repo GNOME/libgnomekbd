2099ee3e6dc36f147cf8834bdaaa9bb3125fae45
Matthias Clasen <mclasen@redhat.com>
2011-01-21 13:41:49 -0500

    Fix clipping problems in the drawing widget
    No-window widgets _must_ call gtk_widget_set_has_window (w, FALSE)
    in _init; else the clipping/transformation logic in GTK+ gets
    confused.

--- libgnomekbd/gkbd-keyboard-drawing.c
+++ libgnomekbd/gkbd-keyboard-drawing.c
@@ -2029,6 +2029,7 @@ gkbd_keyboard_drawing_init (GkbdKeyboardDrawing * drawing)
 	drawing->track_config = 0;
 
 	gtk_widget_set_double_buffered (GTK_WIDGET (drawing), FALSE);
+        gtk_widget_set_has_window (GTK_WIDGET (drawing), FALSE);
 
 	/* XXX: XkbClientMapMask | XkbIndicatorMapMask | XkbNamesMask | XkbGeometryMask */
 	drawing->xkb = XkbGetKeyboard (drawing->display,
